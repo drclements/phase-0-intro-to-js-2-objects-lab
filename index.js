13543fd3e1f12ecb292afba8a0e0ab6bfada964c
Daniel Clements <drclements@liberty.edu>
2022-11-01 22:06:00 -0400

    commit tree 282d5f7e2a2c1d2f3dc76bd042f238fa5f4c4c25

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Write your solution in this file!
+const employee = {
+    name: 'John',
+    streetAddress: "2964 Deep Gap Rd",
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return {...employee,
+    [key]: value
+    }
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue (employee, key, value) {
+    employee.name = "Sam"
+    employee.streetAddress = "12 Broadway"
+    
+    return employee
+}
+
+function deleteFromEmployeeByKey (employee, key) {
+    let newEmployee = {...employee}
+    delete newEmployee.name
+
+    return newEmployee
+}
+
+// function ddestructivelyDeleteFromEmployeeByKey (employee, key) {
+//     delete employee.name
+
+//     return employee
+// }
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee.name
+
+    return employee
+}
